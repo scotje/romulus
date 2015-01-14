506c09c90ddd5b59e08af40c78b59a364afe486b
Jesse <jesse.c.scott@gmail.com>
2015-01-13 20:08:42 -0800

    Initial import of Dart boilerplate.

---
+++ web/main.dart
@@ -0,0 +1,20 @@
+// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'dart:html' as html;
+
+import 'package:stagexl/stagexl.dart';
+
+void main() {
+  // setup the Stage and RenderLoop
+  var canvas = html.querySelector('#stage');
+  var stage = new Stage(canvas);
+  var renderLoop = new RenderLoop();
+  renderLoop.addStage(stage);
+  
+  // draw a red circle
+  var shape = new Shape();
+  shape.graphics.circle(100, 100, 60);
+  shape.graphics.fillColor(Color.Red);
+  stage.addChild(shape);
+}
