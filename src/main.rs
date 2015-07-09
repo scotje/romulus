d48083cac69d345846313efe98c58345580825dc
jesse <jesse@ysera>
2015-07-08 21:03:24 -0700

    Added basic ability to move the box around with the arrow keys.

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,18 @@ extern crate glutin_window;
 extern crate opengl_graphics;
 
 use piston::window::WindowSettings;
+use piston::window::Window;
 use piston::event::*;
-use glutin_window::GlutinWindow as Window;
+use piston::input::*;
+use glutin_window::GlutinWindow;
 use opengl_graphics::{ GlGraphics, OpenGL };
 
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
-    rotation: f64   // Rotation for the square.
+    rotation: f64,   // Rotation for the square.
+
+    box_x: f64,
+    box_y: f64,
 }
 
 impl App {
@@ -22,7 +27,7 @@ impl App {
 
         let square = rectangle::square(0.0, 0.0, 50.0);
         let rotation = self.rotation;
-        let (x, y) = ((args.width / 2) as f64, (args.height / 2) as f64);
+        let (x, y) = (self.box_x, self.box_y);
 
         self.gl.draw(args.viewport(), |c, gl| {
             // Clear the screen.
@@ -41,13 +46,25 @@ impl App {
         // Rotate 2 radians per second.
         self.rotation += 2.0 * args.dt;
     }
+
+    fn keypress(&mut self, key: keyboard::Key) {
+        use piston::input::keyboard::Key::*;
+
+        match key {
+            Up    => { self.box_y -= 10.0; },
+            Down  => { self.box_y += 10.0; },
+            Left  => { self.box_x -= 10.0; },
+            Right => { self.box_x += 10.0; },
+            _     => println!("other"),
+        }
+    }
 }
 
 fn main() {
     let opengl = OpenGL::_3_2;
 
     // Create an Glutin window.
-    let window = Window::new(
+    let window = GlutinWindow::new(
         opengl,
         WindowSettings::new(
             "romulus",
@@ -56,10 +73,14 @@ fn main() {
         .exit_on_esc(true)
     );
 
+    //println!("window size: {}x{}", window.size().width, window.size().height);
+
     // Create a new game and run it.
     let mut app = App {
         gl: GlGraphics::new(opengl),
-        rotation: 0.0
+        rotation: 0.0,
+        box_x: (window.size().width / 2) as f64,
+        box_y: (window.size().height / 2) as f64,
     };
 
     for e in window.events() {
@@ -69,6 +90,13 @@ fn main() {
 
         if let Some(u) = e.update_args() {
             app.update(&u);
+        }
+
+        if let Some(b) = e.press_args() {
+            match b {
+                Button::Keyboard(key) => app.keypress(key),
+                Button::Mouse(button) => println!("mouse button press"),
+            }
         }
     }
 }
